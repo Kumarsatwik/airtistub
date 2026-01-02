30d4104e4a2737d795b40d4ed327cb4910c65759
kumarsatwik <kumarsatwik6@gmail.com>
2026-01-03 00:22:52 +0530

    feat(schema): add project table with user relation
    Add new project table schema with fields for project details and a foreign key reference to users table

--- config/schema.tsx
+++ config/schema.tsx
@@ -1,8 +1,20 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   credits: integer().default(5),
+});
+
+export const projectTable = pgTable("project", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  projectId: varchar({ length: 255 }).notNull().unique(),
+  userInput: varchar().notNull(),
+  deviceType: varchar({ length: 255 }).notNull(),
+  createdOn: date().defaultNow(),
+  config: json(),
+  userId: varchar({ length: 255 })
+    .references(() => usersTable.email)
+    .notNull(),
 });
