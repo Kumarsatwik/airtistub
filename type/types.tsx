2ec1de8b46e2669ca725be5d56f462c096dc2834
kumarsatwik <kumarsatwik6@gmail.com>
2026-01-03 13:16:53 +0530

    feat(types): add ProjectType interface for project data

--- type/types.tsx
+++ type/types.tsx
@@ -4,3 +4,13 @@ export const DeviceType = {
   desktop: "desktop",
   web: "web",
 };
+
+export type ProjectType = {
+  id: number;
+  projectId: string;
+  device: string;
+  userInput: string;
+  createdOn: string;
+  projectName?: string;
+  theme?: string;
+};
